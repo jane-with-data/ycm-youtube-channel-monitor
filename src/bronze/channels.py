320aaf9e49bbf5b813a736b64647ce2df56c2b4b
jane-with-data <ngan.nk.data@gmail.com>
2026-03-18 19:38:18 +0700

    push source follow new data architecture v2

--- src/bronze/channels.py
+++ src/bronze/channels.py
@@ -1,139 +1,75 @@
-import pandas as pd
-import pyarrow
-import hashlib
 import json
+import pandas
+import requests
 import uuid
 from datetime import datetime, timezone
-from pathlib import Path
+from config.settings import YOUTUBE_API_V3, YOUTUBE_API_KEY
 from config.constants import PATH
-pd.set_option("display.max_rows", None, "display.max_columns", None)
-pd.set_option("display.max_colwidth", None)
+from pathlib import Path
+from utils.logger import get_logger
 
-# Flow
-# read raw data -> process metadata -> process payload -> generate additional columns -> merge -> rename columns -> just keep necessary columns -> write to parquet
-# TODO 
-# Hash items instead of whole  payload.
-
+# TODO:
 class Channels:
     def __init__(self):
-        
-        # Gen File Path
-        self.dir_data_raw = PATH['DIR_DATA_RAW']
+        self.YOUTUBE_API_V3 = YOUTUBE_API_V3
+        self.YOUTUBE_API_KEY = YOUTUBE_API_KEY
         self.dir_data_bronze = PATH['DIR_DATA_BRONZE']
+        self.logger = get_logger()
+        
+    def _fetch_channel_data_by_channel_id(self, channel_id: str) -> dict:
+        """Get data by channel id using Youtube API V3"""
+        
+        BASE_URL = f"{self.YOUTUBE_API_V3}/channels/" 
+        params = {
+            "part": "snippet,contentDetails,statistics",
+            "id": channel_id,
+            "maxResults": 50,
+            "key": self.YOUTUBE_API_KEY
+        }
+        
+        response = requests.get(BASE_URL, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    def _gen_metadata(self) -> dict:    
+        metadata = {
+            "_bronze_id": str(uuid.uuid4()),
+            "_landed_at": datetime.now(timezone.utc).isoformat(),
+            "_source_system": "API",
+            "_source_file": "youtube_api_v3",    
+        }
+        return metadata        
+        
+    def _save_to_json_file(self, data: dict) -> None:
         
         year = datetime.now(timezone.utc).strftime('%Y')
         month = datetime.now(timezone.utc).strftime('%m')
         day = datetime.now(timezone.utc).strftime('%d')
         hour = datetime.now(timezone.utc).strftime('%H')
+
+        file_name = f"channels.jsonl"
+        file_path_not_include_file_name = self.dir_data_bronze / "youtube" / "channels" / f"year={year}" / f"month={month}" / f"day={day}" / f"hour={hour}" 
+        file_path_not_include_file_name.mkdir(parents=True, exist_ok=True)
+        file_path = file_path_not_include_file_name / file_name
+
+        # if rerun pipeline within the same hour - overwrite file
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write(json.dumps(data, ensure_ascii=True) + "\n")
+        self.logger.info(f"Save json successful at {file_path}")
         
-        file_name_raw = f"channels.jsonl"
-        self.file_path_raw = self.dir_data_raw / "youtube" / "channels" / f"year={year}" / f"month={month}" / f"day={day}" / f"hour={hour}" / file_name_raw
+    def run_pipeline(self, channel_id: str) -> None:
+        metadata = self._gen_metadata()
+        channel_data = self._fetch_channel_data_by_channel_id(channel_id)
         
-        file_name_bronze = f"channels.parquet"
-        self.file_path_bronze = self.dir_data_bronze / "youtube" / "channels" / f"year={year}" / f"month={month}" / f"day={day}" / f"hour={hour}" / file_name_bronze
-    
-    def _gen_checksum(self, data: dict) -> str:
-        """
-        Generate a checksum using SHA256
-        """
-        payload_str = json.dumps(data, sort_keys=True, ensure_ascii=True)
-        return hashlib.sha256(payload_str.encode()).hexdigest()
-    
-    def _read_raw_layer(self, _file_path_raw = None) -> pd.DataFrame:
-        if _file_path_raw is None:
-            _file_path_raw = self.file_path_raw
-            
-        data = pd.read_json(_file_path_raw, lines=True)
-        return data
-    
-    def _transform(self, df_raw: pd.DataFrame) -> pd.DataFrame:
-        len_df_raw = len(df_raw)
+        insert_data = {
+            "metadata": metadata,
+            "payload": channel_data               
+        }
+
+        self._save_to_json_file(insert_data)
+        self.logger.info(f"Pipeline run successful with channel_id [{channel_id}]")
         
-        # Process metadata: Flatten metadata column
-        df_metadata = pd.json_normalize(df_raw['metadata'])
-        
-        # Process payload: Flatten payload column -> items[0]
-        items_dict = df_raw['payload'].apply(lambda row: row.get("items", [{}])[0])
-        df_items = pd.json_normalize(items_dict)
-        
-        # Generate system columns
-        df_add_cols = pd.DataFrame({
-            '_bronze_id': [str(uuid.uuid4()) for _ in range(len_df_raw)],
-            '_ingested_at': [datetime.now(timezone.utc).isoformat() for _ in range(len_df_raw)],
-            '_checksum': [self._gen_checksum(item) for item in items_dict],
-            '_modified_at': [datetime.now(timezone.utc).isoformat() for _ in range(len_df_raw)],
-            '_is_deleted': [False for _ in range(len_df_raw)],
-        })
-        
-        # Merge metadata, items, and additional columns
-        df_merged = pd.concat([df_metadata, df_items, df_add_cols], axis=1)
-        
-        # Rename columns
-        df_merged = df_merged.rename(columns={
-            'id': 'channel_id',
-            'snippet.title': 'title',
-            'snippet.description': 'description',
-            'snippet.customUrl': 'custom_url',
-            'snippet.publishedAt': 'published_at',
-            'snippet.thumbnails.default.url': 'thumbnails_default_url',
-            'snippet.thumbnails.default.width': 'thumbnails_default_width',
-            'snippet.thumbnails.default.height': 'thumbnails_default_height',
-            'snippet.thumbnails.medium.url': 'thumbnails_medium_url',
-            'snippet.thumbnails.medium.width': 'thumbnails_medium_width',
-            'snippet.thumbnails.medium.height': 'thumbnails_medium_height',
-            'snippet.thumbnails.high.url': 'thumbnails_high_url',
-            'snippet.thumbnails.high.width': 'thumbnails_high_width',
-            'snippet.thumbnails.high.height': 'thumbnails_high_height',
-            'contentDetails.relatedPlaylists.uploads': 'playlist_id_upload',
-            'statistics.viewCount': 'view_count',
-            'statistics.subscriberCount': 'subscriber_count',
-            'statistics.videoCount': 'video_count',
-        })
-        
-        # Just keep neccesary columns
-        df_merged_keep_cols = [
-            '_bronze_id',
-            '_source_system',
-            '_source_file',
-            '_landed_at',
-            '_ingested_at',
-            '_checksum',
-            '_modified_at',
-            '_is_deleted',
-            'channel_id',
-            'playlist_id_upload',
-            'title',
-            'description',
-            'custom_url',
-            'published_at',
-            'thumbnails_default_url',
-            'thumbnails_default_width',
-            'thumbnails_default_height',
-            'thumbnails_medium_url',
-            'thumbnails_medium_width',
-            'thumbnails_medium_height',
-            'thumbnails_high_url',
-            'thumbnails_high_width',
-            'thumbnails_high_height',
-            'view_count',
-            'subscriber_count',
-            'video_count'
-        ]
-        df_merged = df_merged[df_merged_keep_cols]
-        return df_merged
-                
-    def _save_to_parquet(self, df_save: pd.DataFrame, _file_path_bronze = None) -> None:
-        if _file_path_bronze is None:
-            _file_path_bronze = self.file_path_bronze
-        
-        _file_path_bronze.parent.mkdir(parents=True, exist_ok=True)
-        df_save.to_parquet(_file_path_bronze, index=False)
-    
-    def run_pipeline(self) -> None:
-        df_raw = self._read_raw_layer()
-        df_transformed = self._transform(df_raw)
-        self._save_to_parquet(df_transformed)
-        
+
 if __name__ == "__main__":
     channel = Channels()
-    channel.run_pipeline()
+    data = channel.run_pipeline("UCM9KgI3IytaTL9hr0vhXxuQ")
